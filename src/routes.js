61d36be7dca504bde46e46da03cc77430f91a6e0
Ramazan Bakır <48602486+RamazanBakir@users.noreply.github.com>
2021-05-14 12:51:06 +0300

    Routes Menu

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Material Dashboard React - v1.10.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 // @material-ui/icons
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
@@ -55,9 +38,9 @@ const dashboardRoutes = [
   },
   {
     path: "/table",
-    name: "Table List",
-    rtlName: "قائمة الجدول",
-    icon: "content_paste",
+    name: "Default Mailbox",
+    rtlName: "صندوق البريد الافتراضي",
+    icon: "mail",
     component: TableList,
     layout: "/admin",
   },
